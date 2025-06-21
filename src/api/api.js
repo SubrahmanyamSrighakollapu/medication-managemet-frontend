6d20903737a9f6be021bbafdfa73459dfbf9b54b
Subrahmanyam1563 <subrahmayam.s@botshreyasi.com>
2025-06-22 04:33:47 +0530

    Trigger redeploy

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+  console.log('API Base URL:', process.env.REACT_APP_API_URL);
+
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
